8138788f4ae4b4d37ac66c56c2329e772c374f7b
nesdebie <ndebiesm@gmail.com>
2025-12-02 10:06:55 +0100

    upgrade prject readability

--- train.go
+++ train.go
@@ -12,22 +12,59 @@ import (
 	"gonum.org/v1/plot/vg"
 )
 
+
 func estimatePrice(mileage, theta0, theta1 float64) float64 {
 	return theta0 + theta1*mileage
 }
+
 
 func errorAndExit(reason string) {
 	fmt.Println("error:", reason)
 	os.Exit(1)
 }
 
-func main() {
-	file, err := os.Open("data.csv")
+
+func createModelFile(theta0, theta1 float64) {
+	f, err := os.Create("model.txt")
 	if err != nil {
 		errorAndExit(err.Error())
 	}
-	defer file.Close()
+	defer f.Close()
+	fmt.Fprintf(f, "theta0=%.10f\n", theta0)
+	fmt.Fprintf(f, "theta1=%.10f\n", theta1)
+    fmt.Println("---------------------------\n")
+	fmt.Println("Model saved to ./model.txt")
+}
 
+
+func fillGraphicPlot(pts plotter.XYs, theta0, theta1 float64, input string) {
+	p := plot.New()
+	p.Title.Text = "Car Price vs Mileage"
+	p.X.Label.Text = "Mileage (km)"
+	p.Y.Label.Text = "Price"
+
+	scatter, err := plotter.NewScatter(pts)
+	if err != nil {
+		errorAndExit(err.Error())
+	}
+	p.Add(scatter)
+
+	line := plotter.NewFunction(func(x float64) float64 {
+		return theta0 + theta1*x
+	})
+	line.Color = color.RGBA{R: 255, A: 255}
+	line.Width = vg.Points(2)
+	p.Add(line)
+
+    filePath := "img/price_vs_mileage_" + input + ".png"
+	if err := p.Save(8*vg.Inch, 5*vg.Inch, filePath); err != nil {
+		errorAndExit(err.Error())
+	}
+	fmt.Println("Plot created ! Check ", filePath, "\n")
+}
+
+
+func parseData(file *os.File) ([]float64, []float64, plotter.XYs) {
 	reader := csv.NewReader(file)
 	rows, err := reader.ReadAll()
 	if err != nil {
@@ -57,25 +94,16 @@ func main() {
 	if len(mileages) == 0 {
 		errorAndExit("no data")
 	}
+	return mileages, prices, pts
+}
 
-	minMil, maxMil := mileages[0], mileages[0]
-	for _, v := range mileages {
-		if v < minMil {
-			minMil = v
-        }
-		if v > maxMil {
-			maxMil = v
-        }
-	}
 
-	normMil := make([]float64, len(mileages))
-	for i := range mileages {
-		normMil[i] = (mileages[i] - minMil) / (maxMil - minMil)
-    }
-
+func linearRegression(mileages, prices, normMil []float64, minMil, maxMil float64) (string, float64, float64) {
 	m := float64(len(mileages))
 	theta0 := 0.0
 	theta1 := 0.0
+	denormalizedTheta0 := 0.0
+	denormalizedTheta1 := 0.0
 	learningRate := 0.1
 
 	fmt.Print("Amount of iterations: ")
@@ -101,47 +129,41 @@ func main() {
 		theta0 -= learningRate * (1/m) * sum0
 		theta1 -= learningRate * (1/m) * sum1
     
-        tmpTheta1 := theta1 / (maxMil - minMil)
-        fmt.Printf("Iteration #%d: Theta0 = %.5f, Theta1 = %.5f\n", i + 1, theta0 - tmpTheta1*minMil, theta1 / (maxMil - minMil))
+        denormalizedTheta1 = theta1 / (maxMil - minMil)
+		denormalizedTheta0 = theta0 - denormalizedTheta1*minMil
+        fmt.Printf("Iteration #%d: Theta0 = %.5f, Theta1 = %.5f\n", i + 1, denormalizedTheta0, denormalizedTheta1)
 	}
+	return input, denormalizedTheta0, denormalizedTheta1
+}
 
-	realTheta1 := theta1 / (maxMil - minMil)
-	realTheta0 := theta0 - realTheta1*minMil
 
-	f, err := os.Create("model.txt")
+func main() {
+	file, err := os.Open("data.csv")
 	if err != nil {
 		errorAndExit(err.Error())
 	}
-	defer f.Close()
-	fmt.Fprintf(f, "theta0=%.10f\n", realTheta0)
-	fmt.Fprintf(f, "theta1=%.10f\n", realTheta1)
-    fmt.Println("---------------------------\n")
-	fmt.Println("Model saved to ./model.txt")
+	defer file.Close()
+	mileages, prices, pts := parseData(file)
 
-	p := plot.New()
-	p.Title.Text = "Car Price vs Mileage"
-	p.X.Label.Text = "Mileage (km)"
-	p.Y.Label.Text = "Price"
-
-	scatter, err := plotter.NewScatter(pts)
-	if err != nil {
-		errorAndExit(err.Error())
+	minMil, maxMil := mileages[0], mileages[0]
+	for _, v := range mileages {
+		if v < minMil {
+			minMil = v
+        }
+		if v > maxMil {
+			maxMil = v
+        }
 	}
-	p.Add(scatter)
 
-	line := plotter.NewFunction(func(x float64) float64 {
-		return realTheta0 + realTheta1*x
-	})
-	line.Color = color.RGBA{R: 255, A: 255}
-	line.Width = vg.Points(2)
-	p.Add(line)
+	normMil := make([]float64, len(mileages))
+	for i := range mileages {
+		normMil[i] = (mileages[i] - minMil) / (maxMil - minMil)
+    }
 
-    filePath := "img/price_vs_mileage_" + input + ".png"
-	if err := p.Save(8*vg.Inch, 5*vg.Inch, filePath); err != nil {
-		errorAndExit(err.Error())
-	}
-	fmt.Println("Plot created ! Check ", filePath, "\n")
+	input, theta0, theta1 := linearRegression(mileages, prices, normMil, minMil, maxMil)
+	createModelFile(theta0, theta1)
+	fillGraphicPlot(pts, theta0, theta1, input)
 
     fmt.Println("-----theta0 and theta1-----")
-	fmt.Printf("Theta0 = %.5f\nTheta1 = %.5f\n", realTheta0, realTheta1)
+	fmt.Printf("Theta0 = %.5f\nTheta1 = %.5f\n", theta0, theta1)
 }
